7f788813f37cabee38542dc82abd2aa616bb7724
pratikshmalik <pratikshmalik@gmail.com>
2025-10-07 03:08:38 +0530

    Create buySellstoc.java

---
+++ buySellstoc.java
@@ -0,0 +1,24 @@
+public class buySellstock {
+    public static int buySellstock(int price[]){
+        int buyingPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i=0; i<price.length; i++){
+            if (buyingPrice<price[i]){
+                int profit = price[i] - buyingPrice;
+                maxProfit= Math.max(maxProfit, profit);
+
+            }
+            else{
+                buyingPrice= price[i];
+            }
+          
+
+        }
+
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int price[] = {7,1,5,3,6,4};
+        System.out.println(buySellstock(price));
+    }
+}
